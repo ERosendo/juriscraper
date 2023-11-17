04b7d089f6c6136ad3872cba7800529738eaf1c6
Eduardo Rosendo <eduardojra96@gmail.com>
2023-11-17 00:20:32 -0400

    feat(pacer): Add main function

--- juriscraper/pacer/acms_attachment_page.py
+++ juriscraper/pacer/acms_attachment_page.py
@@ -1,14 +1,16 @@
-
 import json
+import pprint
+import sys
+
 from typing import Dict
 
 from ..lib.log_tools import make_default_logger
 
-from ..lib.log_tools import make_default_logger
 from .reports import BaseReport
 
 
 logger = make_default_logger()
+
 
 class ACMSAttachmentPage(BaseReport):
     """Parse ACMS attachment pages' JSON."""
@@ -28,7 +30,6 @@ class ACMSAttachmentPage(BaseReport):
         """
         self._acms_json = json.loads(text)
 
-
     @property
     def data(self) -> Dict:
         case_details = self._acms_json["caseDetails"]
@@ -42,12 +43,35 @@ class ACMSAttachmentPage(BaseReport):
         for row in self._acms_json["docketEntryDocuments"]:
             result["attachments"].append(
                 {
-                    "attachment_number": int(row['documentNumber']),
-                    "description": row['name'],
-                    "page_count": row['billablePages'],
+                    "attachment_number": int(row["documentNumber"]),
+                    "description": row["name"],
+                    "page_count": row["billablePages"],
                     "pacer_doc_id": docket_entry["docketEntryId"],
-                    "acms_document_guid": row['docketDocumentDetailsId']
+                    "acms_document_guid": row["docketDocumentDetailsId"],
                 }
             )
 
         return result
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(
+            "Usage: python -m juriscraper.pacer.acms_attachment_page filepath"
+        )
+        print("Please provide a path to an HTML file to parse.")
+        sys.exit(1)
+    report = ACMSAttachmentPage(
+        "cand"
+    )  # Court ID is only needed for querying.
+    filepath = sys.argv[1]
+    print(f"Parsing HTML file at {filepath}")
+    with open(filepath) as f:
+        text = f.read()
+    report._parse_text(text)
+    print([x for x in report._acms_json])
+    pprint.pprint(report.data, indent=2)
+
+
+if __name__ == "__main__":
+    main()
